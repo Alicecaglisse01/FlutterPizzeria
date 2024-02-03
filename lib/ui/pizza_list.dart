20582a1b7eb875b9594445bee1fcc9c521903d1a
Alicecaglisse01 <alice0404.moisan@gmail.com>
2024-02-03 02:03:22 +0100

    TD 3

--- lib/ui/pizza_list.dart
+++ lib/ui/pizza_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pizzeria/ui/pizza_details.dart';
+import 'package:pizzeria/ui/share/buy_button_widget.dart';
 
 import '../models/pizza.dart';
 import '../models/pizza_data.dart';
@@ -48,55 +50,67 @@ class _PizzaListState extends State<PizzaList> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            ListTile(
-              title: Text(pizza.title),
-              subtitle: Text(pizza.garniture),
-              leading: Icon(Icons.local_pizza),
-            ),
-            Image.asset(
-              'assets/images/pizzas/${pizza.image}',
-              height: 120,
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width,
-              fit: BoxFit.fitWidth,
-            ),
-            Text(pizza.garniture),
-            Container(
-              padding: const EdgeInsets.all(4.0),
-              child: Text(pizza.garniture),
-            ),
-            ElevatedButton(
-              child: Text("Commander"),
-              onPressed: () {
-                print('Commander une pizza');
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PizzaDetails(pizza),
+                    ),
+                );
               },
+              child: _buildPizzaDetails(pizza),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.red.shade800)
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.shopping_cart),
-                      SizedBox(width: 5),
-                      Text("Commander"),
-                    ],
-                  ),
-                  onPressed: () {
-                    print('Commander une pizza');
-                  },
-                )
-              ],
-            )
-
-          ],
-        )
+            BuyButtonWidget(),
+            ],
+        ),
     );
+  }
+
+  _buildPizzaDetails(Pizza pizza) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ListTile(
+          title: Text(pizza.title),
+          subtitle: Text(pizza.garniture),
+          leading: Icon(Icons.local_pizza),
+        ),
+        Image.asset(
+          'assets/images/pizzas/${pizza.image}',
+          height: 120,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.fitWidth,
+        ),
+        Container(
+          padding: const EdgeInsets.all(4.0),
+          child: Text(pizza.garniture),
+        ),
+      ],
+    );
+  }
+
+  _buildBuyButton() {
+    return
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.red.shade800)
+            ),
+            child: Row(
+              children: [
+                Icon(Icons.shopping_cart),
+                SizedBox(width: 5),
+                Text("Commander"),
+              ],
+            ),
+            onPressed: (){
+              print('Commander une pizza');
+            },
+          )
+        ],
+      );
   }
 }
